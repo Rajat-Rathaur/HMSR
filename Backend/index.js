1704748d704f566949b32e886ade08a47361952a
sahil-1012 <sahil10.jai@gmail.com>
2024-01-01 14:09:48 +0530

    test

--- Backend/index.js
+++ Backend/index.js
@@ -15,29 +15,29 @@ const PORT = process.env.PORT || 4000;
 // initializeDatabase();
 
 
-const startRoutes = require("./Routes/startRoutes");
-const hosteliteRoutes = require("./Routes/hosteliteRoutes");
-const loginRoutes = require("./Routes/loginRoutes");
-const branchRoutes = require("./Routes/branchRoutes");
-const adminRoutes = require("./Routes/adminRoutes");
-const servicesRoutes = require("./Routes/servicesRoutes");
-const employeeRoutes = require("./Routes/employeeRoutes");
-const feedsRoutes = require("./Routes/feedsRoutes");
-const paymentRoutes = require("./Routes/paymentRoutes");
+// const startRoutes = require("./Routes/startRoutes");
+// const hosteliteRoutes = require("./Routes/hosteliteRoutes");
+// const loginRoutes = require("./Routes/loginRoutes");
+// const branchRoutes = require("./Routes/branchRoutes");
+// const adminRoutes = require("./Routes/adminRoutes");
+// const servicesRoutes = require("./Routes/servicesRoutes");
+// const employeeRoutes = require("./Routes/employeeRoutes");
+// const feedsRoutes = require("./Routes/feedsRoutes");
+// const paymentRoutes = require("./Routes/paymentRoutes");
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-app.use("/", startRoutes);
+// app.use("/", startRoutes);
 
-app.use("/api/login", loginRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/branch", branchRoutes);
-app.use("/api/employee", employeeRoutes);
-app.use("/api/hostelite", hosteliteRoutes);
-app.use("/api/services", servicesRoutes);
-app.use("/api/feeds", feedsRoutes);
-app.use("/api/payment", paymentRoutes);
+// app.use("/api/login", loginRoutes);
+// app.use("/api/admin", adminRoutes);
+// app.use("/api/branch", branchRoutes);
+// app.use("/api/employee", employeeRoutes);
+// app.use("/api/hostelite", hosteliteRoutes);
+// app.use("/api/services", servicesRoutes);
+// app.use("/api/feeds", feedsRoutes);
+// app.use("/api/payment", paymentRoutes);
 
 
 app.listen(PORT, () => {
