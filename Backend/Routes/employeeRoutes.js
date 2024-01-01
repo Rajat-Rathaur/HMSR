6b59efeaa56e5816abb62cd9b61b4fe8cee59898
sahil-1012 <sahil10.jai@gmail.com>
2024-01-01 15:29:41 +0530

    final

--- Backend/Routes/employeeRoutes.js
+++ Backend/Routes/employeeRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const { addEmployee, deleteEmployee, getEmployee } = require("../Operations/employeeOperation");
+const { addEmployee, deleteEmployee, getEmployee } = require("../Operations/employeeOperations");
 
 
 const addEmployeeValidations = require("../Validations/EmployeeValidations/addEmployeeValidation");
