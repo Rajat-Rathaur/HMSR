5feb4a3612778d461b6a5406a3d22d5fd13a0661
sahil-1012 <sahil10.jai@gmail.com>
2024-01-01 16:02:22 +0530

    try

--- Backend/Routes/hosteliteRoutes.js
+++ Backend/Routes/hosteliteRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const { addHostelite, getHostelite, updateHostelite, updatedHostelitePassword, deleteHostelite } = require("../Operations/hosteliteOperation");
+const { addHostelite, getHostelite, updateHostelite, updatedHostelitePassword, deleteHostelite } = require("../Operations/hosteliteOperations");
 
 const requireAuth = require("../Middlewares/reqAuth");
 
