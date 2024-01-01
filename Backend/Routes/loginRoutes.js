7a4bdba4715c776e96d5a0573269ca3cd9bce538
sahil-1012 <sahil10.jai@gmail.com>
2024-01-01 14:24:58 +0530

    Final

--- Backend/Routes/loginRoutes.js
+++ Backend/Routes/loginRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 // const {  checkHosteliteCredentials } = require("../Operations/HosteliteOperations");
-const { checkEmployeeCredentials } = require("../Operations/employeeOperations");
+// const { checkEmployeeCredentials } = require("../Operations/employeeOperations");
 const loginValidations = require("../Validations/loginValidations");
 
 
