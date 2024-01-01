70eb8d6c2f0a8f0a3dc289c7a13a96c11de6df06
sahil-1012 <sahil10.jai@gmail.com>
2024-01-01 14:07:54 +0530

    Final

--- Backend/index.js
+++ Backend/index.js
@@ -2,42 +2,42 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
-// const cors = require("cors");
-// const createTables = require("./SQLUtilities/createTables");
-// const createConstraints = require("./SQLUtilities/constraints");
+const cors = require("cors");
+const createTables = require("./SQLUtilities/createTables");
+const createConstraints = require("./SQLUtilities/constraints");
 
 const PORT = process.env.PORT || 4000;
-// const initializeDatabase = async () => {
-//     await createTables();
-//     await createConstraints();
-// };
+const initializeDatabase = async () => {
+    await createTables();
+    await createConstraints();
+};
 
-// initializeDatabase();
+initializeDatabase();
 
 
-// const startRoutes = require("./Routes/startRoutes");
-// const hosteliteRoutes = require("./Routes/hosteliteRoutes");
-// const loginRoutes = require("./Routes/loginRoutes");
-// const branchRoutes = require("./Routes/branchRoutes");
-// const adminRoutes = require("./Routes/adminRoutes");
-// const servicesRoutes = require("./Routes/servicesRoutes");
-// const employeeRoutes = require("./Routes/employeeRoutes");
-// const feedsRoutes = require("./Routes/feedsRoutes");
-// const paymentRoutes = require("./Routes/paymentRoutes");
+const startRoutes = require("./Routes/startRoutes");
+const hosteliteRoutes = require("./Routes/hosteliteRoutes");
+const loginRoutes = require("./Routes/loginRoutes");
+const branchRoutes = require("./Routes/branchRoutes");
+const adminRoutes = require("./Routes/adminRoutes");
+const servicesRoutes = require("./Routes/servicesRoutes");
+const employeeRoutes = require("./Routes/employeeRoutes");
+const feedsRoutes = require("./Routes/feedsRoutes");
+const paymentRoutes = require("./Routes/paymentRoutes");
 
-// app.use(express.json());
-// app.use(cors());
+app.use(express.json());
+app.use(cors());
 
-// app.use("/", startRoutes);
+app.use("/", startRoutes);
 
-// app.use("/api/login", loginRoutes);
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/branch", branchRoutes);
-// app.use("/api/employee", employeeRoutes);
-// app.use("/api/hostelite", hosteliteRoutes);
-// app.use("/api/services", servicesRoutes);
-// app.use("/api/feeds", feedsRoutes);
-// app.use("/api/payment", paymentRoutes);
+app.use("/api/login", loginRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/branch", branchRoutes);
+app.use("/api/employee", employeeRoutes);
+app.use("/api/hostelite", hosteliteRoutes);
+app.use("/api/services", servicesRoutes);
+app.use("/api/feeds", feedsRoutes);
+app.use("/api/payment", paymentRoutes);
 
 
 app.listen(PORT, () => {
