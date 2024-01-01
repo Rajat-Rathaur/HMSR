3e9d33e75fa1466db4e9b5a597ebbb74dda55380
sahil-1012 <sahil10.jai@gmail.com>
2024-01-01 14:11:28 +0530

    test

--- Backend/index.js
+++ Backend/index.js
@@ -19,25 +19,25 @@ app.use(cors());
 
 
 // const startRoutes = require("./Routes/startRoutes");
-// const hosteliteRoutes = require("./Routes/hosteliteRoutes");
-// const loginRoutes = require("./Routes/loginRoutes");
-// const branchRoutes = require("./Routes/branchRoutes");
-// const adminRoutes = require("./Routes/adminRoutes");
-// const servicesRoutes = require("./Routes/servicesRoutes");
-// const employeeRoutes = require("./Routes/employeeRoutes");
-// const feedsRoutes = require("./Routes/feedsRoutes");
-// const paymentRoutes = require("./Routes/paymentRoutes");
+const hosteliteRoutes = require("./Routes/hosteliteRoutes");
+const loginRoutes = require("./Routes/loginRoutes");
+const branchRoutes = require("./Routes/branchRoutes");
+const adminRoutes = require("./Routes/adminRoutes");
+const servicesRoutes = require("./Routes/servicesRoutes");
+const employeeRoutes = require("./Routes/employeeRoutes");
+const feedsRoutes = require("./Routes/feedsRoutes");
+const paymentRoutes = require("./Routes/paymentRoutes");
 
 // app.use("/", startRoutes);
 
-// app.use("/api/login", loginRoutes);
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/branch", branchRoutes);
-// app.use("/api/employee", employeeRoutes);
-// app.use("/api/hostelite", hosteliteRoutes);
-// app.use("/api/services", servicesRoutes);
-// app.use("/api/feeds", feedsRoutes);
-// app.use("/api/payment", paymentRoutes);
+app.use("/api/login", loginRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/branch", branchRoutes);
+app.use("/api/employee", employeeRoutes);
+app.use("/api/hostelite", hosteliteRoutes);
+app.use("/api/services", servicesRoutes);
+app.use("/api/feeds", feedsRoutes);
+app.use("/api/payment", paymentRoutes);
 
 
 app.listen(PORT, () => {
