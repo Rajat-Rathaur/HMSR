9ea66e7e0627e3de680f2da69a3696f3d16a990e
sahil-1012 <sahil10.jai@gmail.com>
2024-01-01 15:50:17 +0530

    try

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 
 const startRoutes = require("./Routes/startRoutes");
-// const loginRoutes = require("./Routes/loginRoutes");
+const loginRoutes = require("./Routes/loginRoutes");
 const branchRoutes = require("./Routes/branchRoutes");
 const adminRoutes = require("./Routes/adminRoutes");
 const hosteliteRoutes = require("./Routes/hosteliteRoutes");
@@ -31,10 +31,10 @@ const feedsRoutes = require("./Routes/feedsRoutes");
 app.use("/", startRoutes);
 
 // app.use("/api/login", loginRoutes);
-// app.use("/api/branch", branchRoutes);
-// app.use("/api/admin", adminRoutes);
-app.use("/api/employee", employeeRoutes);
 app.use("/api/hostelite", hosteliteRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/employee", employeeRoutes);
+app.use("/api/branch", branchRoutes);
 app.use("/api/services", servicesRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/feeds", feedsRoutes);
